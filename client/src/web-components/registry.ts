84b2c8567fe2091de03ece2cbb29b5607f93eb1e
Noel Felix <nn.felix.25@gmail.com>
2026-04-08 22:46:15 -0700

    Create new Input component

--- client/src/web-components/registry.ts
+++ client/src/web-components/registry.ts
@@ -1,9 +1,11 @@
-import { HabitCard } from "./components/HabitCard/HabitCard.element.js";
 import { Badge } from "./ui/Badge/Badge.element.js";
 import { Button } from "./ui/Button/Button.element.js";
+import { HabitCard } from "./components/HabitCard/HabitCard.element.js";
+import { Input } from "./ui/Input/Input.element.js";
 
 export function registerWebComponents() {
-    customElements.define("habit-card", HabitCard);
     customElements.define("my-badge", Badge);
     customElements.define("my-button", Button);
+    customElements.define("habit-card", HabitCard);
+    customElements.define("my-input", Input);
 }
